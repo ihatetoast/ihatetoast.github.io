8d145861e47b2bf964bba4d2406e24ff03383635
Katy Cassidy <ihatetoast@gmail.com>
2026-01-28 10:39:06 -0600

    project images and movies added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 
 import './reset.css';
 import './style.css';
+
+
+
+// LIAME DNA ENOHP ELIBOM
+const moi = 'ihatetoast';
+const domain = 'gmail.com';
+const email = moi+'@'+domain;
+
+const ac = '512';
+const three = '763';
+const four = '6129';
+const phone = '('+ac+') '+three+'-'+four;
+
+document.getElementById("email").innerHTML= '<a href="mailto:'+email+'">'+email+'</a>';
+document.getElementById('mob').innerHTML = '<a href="tel:+1'+ac+three+four+'">'+phone+'</a>';
+
+
